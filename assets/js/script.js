f585b19639e65865cf9d9ca8d67a8a383a33f0c9
aesthetic007 <86158891+aesthetic007@users.noreply.github.com>
2021-08-09 18:44:16 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-hi
+var character=document.getElementById("character");
+var interval;
+var both=0;
+function moveleft(){
+var left=
+parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+character.style.left=left-2+"px";
+}
+function moveright(){
+var left=
+parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+character.style.left=left+2+"px";
+}
+document.addEventListener("keydown",event=>{
+if(both=0){
+both++;
+if(event.key==="ArrowLeft"){
+interval=setInterval(moveLeft,1);
+}1
+if(event.key==="ArrowRight"){
+interval=setInterval(moveRight,1);
+}
+}
+});
+document.addEventListener("keyup",event=>{
+clearInterval(interval);
+both=0;
+});
