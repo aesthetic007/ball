76050c106f68103ba6012b2aaea537781d40df80
aesthetic007 <86158891+aesthetic007@users.noreply.github.com>
2021-08-10 13:06:08 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,17 @@
-
-var circle=document.getElementById("circle");
-			var upBtn=document.getElementById("upBtn");
-			var downBtn=document.getElementById("downBtn");
-			
-			var rotatevalue=circle.style.transform;
-			var rotateSum;
-			
-			upBtn.onclick=function()
-			{
-				rotateSum=rotatevalue+"rotate(-90deg)";
-				circle.style.transform=rotateSum;
-				rotatevalue=rotateSum;
-			}
-			downBtn.onclick=function()
-			{
-				rotateSum=rotatevalue+"rotate(90deg)";
-				circle.style.transform=rotateSum;
-				rotatevalue=rotateSum;
-			}
-		<audio id="mysong">
-			<source src="image/song.mp3" type="audio/mp3">
-		</audio>
-		var mysong=document.getElementById("mysong");
-		var icon=document.getElementById("icon");
+var character = document.getElementById("character");
+var mousedownID = -1;
+function mousedownLeft(event){
+	if(mousedownID==-1)mousedownID=
+	setInterval(whilemousedownLeft, 2);
+}
+function mouseupLeft(event){
+	if(mousedownID!=-1){
+		clearInterval(mousedownID);
+		mousedownID=-1;
+	}
+}
+	
 		
-		icon.onclick=function(){
-			if(mysong.paused){	
-				mysong.play();
-				icon.src="image/pause.png";
-			}else{
-				mysong.pause();
-				icon.src="image/play.png";
-			}
-			
-		}
+	
+	
+
